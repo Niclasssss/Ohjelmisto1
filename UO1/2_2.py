dc05f1eb1eed5bea842d1b1d494e6441667cd1fd
Niclasssss <niclas.lausama@kotiposti.net>
2026-03-07 21:31:40 +0200

    Funktiot ja perus toiminnallisuus mainiin

--- UO1/2_2.py
+++
@@ -1,5 +0,0 @@
-import math
-radius = float(input("Enter the radius of the circle: "))
-area = radius * radius * math.pi
-pintaala = "The area of the circle is " + str(area)
-print(pintaala)
--- UO1/2_3.py
+++
@@ -1,7 +0,0 @@
-lenght = float(input("Enter the lenght of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-perimeter = lenght + lenght + width + width
-area = lenght * width
-piiri = "The perimeter of the rectangle is " + str(perimeter)
-pintaala = "The area of the rectangle is " + str(area)
-print(piiri + "," + pintaala)
